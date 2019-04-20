26846cf9ad13949201d35acbef70afe9670ce8f9
Carol Sandi <caroladrisf@gmail.com>
2019-04-19 21:22:53 -0600

    Agrego una navegación temporal y el icono de la aplicación

--- tubekids/src/App.js
+++ tubekids/src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Navigation from './components/Navigation';
+import Register from './components/Register';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        
+
+        <div className="container my-5">
+          <Register />
+        </div>
+
       </div>
     );
   }
--- tubekids/src/components/Navigation.js
+++ tubekids/src/components/Navigation.js
@@ -3,9 +3,31 @@ import React, { Component } from 'react';
 class Navigation extends Component {
   render() {
     return (
-        <nav className="navbar navbar-dark bg-primary">
+        <nav className="navbar navbar-expand navbar-dark bg-primary">
             <div className="container">
-                <a className="navbar-brand" href="/"><i className="fas fa-child"></i> Tubekids</a>
+              <a className="navbar-brand" href="/"><i className="fas fa-child"></i> Tubekids</a>
+              <div className="collapse navbar-collapse">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="/register">Register</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/login">Login</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">Account</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">Profiles</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">Playlist</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">Logout</a>
+                  </li>
+                </ul>
+              </div>
             </div>
         </nav>
     );
